60f417d276d23b409afaef337079a145c6cc5146
Le Duong <23660591.duong@student.iuh.edu.vn>
2024-08-13 21:00:44 +0700

    First Commit

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Le Van Duong!");
+        System.out.println("Welcome to the DHKTPM19A!");
+    }
+}
